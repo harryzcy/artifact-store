244981e7d0a5808fc95710650d9a1011e455b18e
harryzcy <harry@harryzheng.com>
2023-07-24 00:45:15 -0500

    Shorten some types

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,13 @@
+use std::{error, fmt, io};
+
 #[derive(Debug)]
 pub enum CreateFileError {
     IoError(std::io::Error),
     AxumError(axum::Error),
 }
 
-impl std::fmt::Display for CreateFileError {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+impl fmt::Display for CreateFileError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             CreateFileError::IoError(e) => write!(f, "IO error: {}", e),
             CreateFileError::AxumError(e) => write!(f, "Axum error: {}", e),
@@ -13,8 +15,8 @@ impl std::fmt::Display for CreateFileError {
     }
 }
 
-impl std::error::Error for CreateFileError {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+impl error::Error for CreateFileError {
+    fn source(&self) -> Option<&(dyn error::Error + 'static)> {
         match self {
             CreateFileError::IoError(e) => Some(e),
             CreateFileError::AxumError(e) => Some(e),
@@ -22,8 +24,8 @@ impl std::error::Error for CreateFileError {
     }
 }
 
-impl From<std::io::Error> for CreateFileError {
-    fn from(e: std::io::Error) -> Self {
+impl From<io::Error> for CreateFileError {
+    fn from(e: io::Error) -> Self {
         Self::IoError(e)
     }
 }
