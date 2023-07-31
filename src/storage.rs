653c3290be1b9ee63060bdd85bdaaa4e2e8e6e0d
harryzcy <harry@harryzheng.com>
2023-07-30 20:30:39 -0500

    Support `@latest` for file downloads

--- src/storage.rs
+++ src/storage.rs
@@ -67,28 +67,15 @@ pub async fn list_artifacts(
     db: &database::Database,
     params: GetArtifactsParams,
 ) -> Result<GetArtifactsResponse, HandleRequestError> {
-    let is_latest = params.commit == "@latest";
-    let commit = if is_latest {
-        db.get_latest_commit(database::GetLatestCommitParams {
-            server: &params.server,
-            owner: &params.owner,
-            repo: &params.repo,
-        })?
-    } else {
-        let exists = db.exists_commit(database::ExistsCommitParams {
+    let commit = get_or_verify_commit(
+        db,
+        GetOrVerifyCommitParams {
             server: &params.server,
             owner: &params.owner,
             repo: &params.repo,
             commit: &params.commit,
-        })?;
-        if !exists {
-            return Err(HandleRequestError::NotFound(format!(
-                "commit {} not found",
-                params.commit
-            )));
-        }
-        params.commit
-    };
+        },
+    )?;
 
     let artifacts = db.list_artifacts(database::GetArtifactsParams {
         server: &params.server,
@@ -185,11 +172,21 @@ pub async fn prepare_download_file(
     db: &database::Database,
     params: DownloadParams,
 ) -> Result<(String, StreamBody<ReaderStream<File>>), HandleRequestError> {
+    let commit = get_or_verify_commit(
+        db,
+        GetOrVerifyCommitParams {
+            server: &params.server,
+            owner: &params.owner,
+            repo: &params.repo,
+            commit: &params.commit,
+        },
+    )?;
+
     let exists = db.exists_artifact(database::ExistsArtifactParams {
         server: &params.server,
         owner: &params.owner,
         repo: &params.repo,
-        commit: &params.commit,
+        commit: &commit,
         path: &params.path,
     })?;
     if !exists {
@@ -201,7 +198,7 @@ pub async fn prepare_download_file(
 
     let path = format!(
         "{}/{}/{}/{}/{}/{}",
-        data_dir, params.server, params.owner, params.repo, params.commit, params.path
+        data_dir, params.server, params.owner, params.repo, commit, params.path
     );
     let file = match File::open(path).await {
         Ok(file) => file,
@@ -215,4 +212,42 @@ pub async fn prepare_download_file(
     let stream: ReaderStream<File> = ReaderStream::new(file);
     let body = StreamBody::new(stream);
     Ok((params.path, body))
+}
+
+#[derive(Clone)]
+pub struct GetOrVerifyCommitParams<'a> {
+    pub server: &'a String,
+    pub owner: &'a String,
+    pub repo: &'a String,
+    pub commit: &'a String,
+}
+
+/// Get the latest commit if `commit` is "@latest", otherwise verify that `commit` exists.
+fn get_or_verify_commit(
+    db: &database::Database,
+    params: GetOrVerifyCommitParams<'_>,
+) -> Result<String, HandleRequestError> {
+    let is_latest = params.commit == "@latest";
+    if is_latest {
+        let commit = db.get_latest_commit(database::GetLatestCommitParams {
+            server: params.server,
+            owner: params.owner,
+            repo: params.repo,
+        })?;
+        return Ok(commit);
+    }
+
+    let exists = db.exists_commit(database::ExistsCommitParams {
+        server: params.server,
+        owner: params.owner,
+        repo: params.repo,
+        commit: params.commit,
+    })?;
+    if !exists {
+        return Err(HandleRequestError::NotFound(format!(
+            "commit {} not found",
+            params.commit
+        )));
+    }
+    Ok(params.commit.clone())
 }
