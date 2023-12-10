ff358af5deb28f26426db7a447ec741b67e1fc69
Chongyi Zheng <git@zcy.dev>
2023-12-10 08:57:29 -0500

    Bring back graceful shutdown (#124)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use std::net::SocketAddr;
-use tokio::net::TcpListener;
-use tracing::info;
+
+use axum::extract::Request;
+use hyper::server::conn::http1;
+use hyper::{body::Incoming, service};
+use hyper_util::rt::TokioIo;
+use signal::unix::SignalKind;
+use tokio::{net::TcpListener, signal, sync::watch};
+use tower_service::Service;
+use tracing::{debug, info};
 
 mod config;
 mod database;
@@ -20,6 +27,82 @@ async fn main() {
     info!(message = "starting server", port = addr.port());
 
     let listener = TcpListener::bind(&addr).await.unwrap();
-    let router = router::router(conf.data_path, conf.artifact_path, db).into_make_service();
-    axum::serve(listener, router).await.unwrap();
+    let app = router::router(conf.data_path, conf.artifact_path, db);
+
+    let (close_tx, close_rx) = watch::channel(());
+
+    loop {
+        let (socket, remote_addr) = tokio::select! {
+            result = listener.accept() => {
+                result.unwrap()
+            }
+            _ = shutdown_signal() => {
+                debug!("signal received, not accepting new connections");
+                break;
+            }
+        };
+        debug!("connection {remote_addr} accepted");
+
+        let tower_service = app.clone();
+        let close_rx = close_rx.clone();
+        tokio::spawn(async move {
+            let socket = TokioIo::new(socket);
+            let hyper_service = service::service_fn(move |request: Request<Incoming>| {
+                tower_service.clone().call(request)
+            });
+
+            let conn = http1::Builder::new()
+                .serve_connection(socket, hyper_service)
+                .with_upgrades();
+            let mut conn = std::pin::pin!(conn);
+
+            loop {
+                tokio::select! {
+                    result = conn.as_mut() => {
+                        if let Err(err) = result {
+                            debug!("failed to serve connection: {err:#}");
+                        }
+                        break;
+                    }
+                    _ = shutdown_signal() => {
+                        debug!("signal received, starting graceful shutdown");
+                        conn.as_mut().graceful_shutdown();
+                    }
+                }
+            }
+
+            debug!("connection {remote_addr} closed");
+            drop(close_rx);
+        });
+    }
+
+    drop(close_rx);
+    drop(listener);
+
+    debug!("waiting for {} tasks to finish", close_tx.receiver_count());
+    close_tx.closed().await;
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
 }
